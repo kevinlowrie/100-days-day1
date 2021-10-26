f2d20b8b0995f2190e46dfe44e5a96c80a7787e2
KevinLowrie <>
2021-10-26 22:34:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#Write your code below this line
+
+print("```\n", "Day 1 - Python Print Function.\n")
+print("The function is declared like this -\n``` ")
+
+print("print('what to print?')\n```")
+
